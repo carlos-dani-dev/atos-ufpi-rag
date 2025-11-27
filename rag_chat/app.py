bcb1e12916314b479ecb5d523a687ad46f6d4394
carlos-dani-dev <carlos.rocha@ufpi.edu.br>
2025-11-27 11:25:43 -0300

    Implementação de um agente de IA simples para definição do tipo de busca (Semântica ou Lexical)

--- rag_chat/app.py
+++ rag_chat/app.py
@@ -1,3 +1,4 @@
+import re
 import os
 from langchain_text_splitters import MarkdownHeaderTextSplitter
 import streamlit as st
@@ -13,10 +14,31 @@ from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains import create_retrieval_chain
 
 
+def search_type_agent(question, aux_prompt):
+    response = llm.invoke(aux_prompt.format_messages(aux_input=question))
+    print("RESPONSE: ", response.content)
+    return response.content
+    
+
+def normalizar_processos(texto):
+    padrao = r"\b\d{5}\.\d{6}/\d{4}-\d{2}\b"
+
+    def normalizar(match):
+        original = match.group(0)
+        normalizado = re.sub(r"[./-]", "", original)
+        return f"{normalizado} (original: {original})"
+
+    return re.sub(padrao, normalizar, texto)
+
+
 def lexical_search_preprocess(question):
     special_char = ['?', '/', '-', ',', '.', ';']
+
+    question = normalizar_processos(question)
+
     for char in special_char:
         question = question.replace(char, f" {char}")
+
     return question
 
 
@@ -39,13 +61,6 @@ with st.sidebar:
     ["deepseek/deepseek-chat-v3.1:free", "qwen/qwen3-235b-a22b:free", "deepseek/deepseek-r1-0528:free", "openai/gpt-oss-120b:free", "openai/gpt-oss-20b:free", "qwen/qwen3-coder:free", "moonshotai/kimi-k2:free", "google/gemini-2.5-pro-exp-03-25", "google/gemma-3-flash-e2b-it:free"],
         index=0,
         help="Escolha o modelo de linguagem que irá gerar as respostas."
-    )
-    
-    search_type_choice = st.selectbox(
-        "🔍 Tipo de Busca",
-        ["Semântica (Qwen 0.6B Embedding)", "Lexical (BM25)"],
-        index=0,
-        help="Escolha entre busca por similaridade vetorial (semântica) ou por palavras-chave (lexical)."
     )
 
     st.markdown("---")
@@ -125,7 +140,29 @@ llm = ChatOpenAI(
     max_tokens=2048,
 )
 
-prompt_template = """
+aux_prompt_template = """
+Você é um assistente especializado em analisar documentos oficiais da Universidade Federal do Piauí (UFPI).
+Analisando o seguinte questionamento {aux_input}. Retorne '0' para caso o questionamento demande
+uma busca LEXICAL do modelo de RAG e retorne '1' para caso o questionamento demande uma busca
+exclusivamente SEMÂNTICA do modelo de RAG.
+
+Uma busca LEXICAL deverá atender à questionamentos que solicitem o acesso a determinada informação
+exclusivamente pelo número do ato normativo ou pelo número do processo contido no texto do ato.
+Um exemplo fixo e bem definido de número de processo é XXXXX.XXXXXX.XXXX-XX em que X é um dígito
+de 0 à 9. Veja exemplos de questionamentos que demandam uma busca LEXICAL do modelo de RAG:
+Exemplo 1: 'O que diz o ato de número 120?'
+Exemplo 2: 'Do que trata o ato 140?'
+
+Uma busca SEMÂNTICA deverá atender à questionamentos que solicitem o acesso a determinada informação
+sem fazer referência ao número do ato normativo ou ao número do processo contido no texto do ato.
+Veja exemplos de questionamentos que demandam uma busca SEMÂNTICA do modelo de RAG:
+Exemplo 1: 'Quais atos citam André Castelo Branco Soares?'
+Exemplo 2: 'O que foi definido, em 2025, em relação ao programa de doutorado do Departamento de Computação?'
+
+De forma concisa e objetiva, atenda ao solicitado inicialmente neste prompt.
+"""
+
+main_prompt_template = """
 Você é um assistente especializado em analisar documentos oficiais da Universidade Federal do Piauí (UFPI).
 Responda à pergunta do usuário com base EXCLUSIVAMENTE no contexto fornecido abaixo.
 Se a informação não estiver no contexto, diga "A informação não foi encontrada nos atos fornecidos.".
@@ -139,33 +176,30 @@ Seja claro, objetivo e cite os números dos atos e processos que basearam sua re
 
 **Resposta Detalhada:**
 """
-prompt = ChatPromptTemplate.from_template(prompt_template)
-document_chain = create_stuff_documents_chain(llm, prompt)
+main_prompt = ChatPromptTemplate.from_template(main_prompt_template)
+aux_prompt = ChatPromptTemplate.from_template(aux_prompt_template)
+
+document_chain = create_stuff_documents_chain(llm, main_prompt)
 retrieval_chain = create_retrieval_chain(retriever, document_chain)
 bm25_retrieval_chain = create_retrieval_chain(bm25_retriever, document_chain)
 
 # --- INTERFACE PRINCIPAL ---
 texto_btn = "Analisar semanticamente"
-if search_type_choice == "Semântica (Qwen 0.6B Embedding)" or search_type_choice == None:
-    question = st.text_input(
-        "Faça sua pergunta sobre os atos da reitoria:",
-        placeholder="Ex: Quem foi nomeado para o cargo de Assistente em Administração em junho de 2022?"
-    )
-    texto_btn = "Analisar semanticamente"
-else:
-    question = st.text_input(
-        "Palavras-chave (Busca Lexical):",
-        placeholder="Ex: '2022', 00017.000057/2025-95",
-        help="Na busca lexical, digite termos literais presentes nos documentos."
-    )
-    texto_btn = "Analisar lexicalmente"
+question = st.text_input(
+    "Faça sua pergunta sobre os atos da reitoria:",
+    placeholder="Ex: Quem foi nomeado para o cargo de Assistente em Administração em junho de 2022?"
+)
+texto_btn = "Analisar"
 
 if st.button(texto_btn, type="primary"):
     if not question:
         st.warning("Por favor, digite uma pergunta.")
     else:
-        if search_type_choice == "Semântica (Qwen 0.6B Embedding)":
-            with st.spinner("Consultando a base de conhecimento e gerando a resposta..."):
+
+        sem_lex_search = search_type_agent(question, aux_prompt)
+
+        if sem_lex_search == "1":
+            with st.spinner("Consultando (Análise Semântica) a base de conhecimento e gerando a resposta..."):
                 try:                
                     docs_with_scores = vector_store.similarity_search_with_score(question, k=k)
                     
@@ -187,15 +221,16 @@ if st.button(texto_btn, type="primary"):
                             with st.expander(f"**{ato}** - Similaridade: {similaridade:.4f}"):
                                 st.caption(f"Processos: {processos}")
                                 st.write(doc.page_content)
-
-                                
+                    
                 except Exception as e:
                     st.error(f"Ocorreu um erro ao processar sua pergunta: {e}")
                     st.error(f"Detalhes do erro: {str(e)}")
-        else:
-            with st.spinner("Consultando a base de conhecimento e gerando a resposta..."):
-                try:
+        elif sem_lex_search == "0":
+            with st.spinner("Consultando (Análise Lexical) a base de conhecimento e gerando a resposta..."):
+                try:            
                     question_proc = lexical_search_preprocess(question)
+                    
+                    print(question_proc)
                     
                     docs = bm25_retriever.get_relevant_documents(question_proc)
                     context = "\n\n".join([d.page_content for d in docs])
@@ -217,7 +252,6 @@ if st.button(texto_btn, type="primary"):
                             with st.expander(f"**{ato}** - Score de similaridade XXX"):
                                 st.caption(f"Processos: {processos}")
                                 st.write(doc.page_content)
-
                                 
                 except Exception as e:
                     st.error(f"Ocorreu um erro ao processar sua pergunta: {e}")
