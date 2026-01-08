ac4afc7a62a9f794cfeb5824b4689b7b75c1e36b
carlos-dani-dev <carlos.rocha@ufpi.edu.br>
2026-01-08 10:37:45 -0300

    Versão final do chatbot, com requirements.txt

--- data_prep/atos_to_csv.py
+++ data_prep/atos_to_csv.py
@@ -5,48 +5,65 @@ import os # Importação mantida, embora não usada no snippet
 
 
 if __name__ == "__main__":
-    lista_de_dfs_2025 = []
-    for ato in os.listdir("atos"):
-        ano = ato.split("_")[0]
-        
-        if int(ano) != 2025: continue
-        
-        mes = ato.split("_")[1]
-        
-        print(f"Ano: {ano} e Mês: {mes}")
-        
-        filepath = f"./atos/{ato}"
-
-        doc = fitz.open(filepath)
-        num_pages = doc.page_count
-        doc.close()
-
-        lista_tabelas_camelot = camelot.read_pdf(
-            filepath, 
-            pages=f"2-{num_pages}", 
-            flavor='lattice'
-        )
-
-        print(f"Número de tabelas encontradas: {lista_tabelas_camelot.n}")
-
-        for i, tabela_atual in enumerate(lista_tabelas_camelot):
-            print(f"\n--- Tabela {i+1} ---")
+    for ano in [2021, 2022, 2023, 2024, 2025]:
+        lista_de_dfs = []
+        for ato in os.listdir("atos"):
+            ano_file = ato.split("_")[0]
             
-            df_atual = tabela_atual.df 
-            print(df_atual.head())
+            if int(ano_file) != ano: continue
             
-            lista_de_dfs_2025.append(df_atual) 
+            mes = ato.split("_")[1]
             
-    tabela_final = pd.concat(lista_de_dfs_2025, ignore_index=True)
-    tabela_final.columns = ["Ato", "Data", "Envolvido", "Texto"]
+            print(f"Ano: {ano} e Mês: {mes}")
+            
+            filepath = f"./atos/{ato}"
 
-    nome_arquivo_csv = "./atos_csv/atos_2025.csv"
-    nome_arquivo_excel = "./atos_excel/atos_2025.xlsx"
+            doc = fitz.open(filepath)
+            num_pages = doc.page_count
+            doc.close()
 
-    tabela_final.to_csv(nome_arquivo_csv, index=False, encoding='utf-8')
-    tabela_final.to_excel("atos_2025.xlsx")
+            lista_tabelas_camelot = camelot.read_pdf(
+                filepath, 
+                pages=f"2-{num_pages}", 
+                flavor='lattice'
+            )
 
-    print("\n-------------------------------------")
-    print(f"Total de linhas na tabela final: {len(tabela_final)}")
-    print(f"Arquivo .csv final salvo como: {nome_arquivo_csv}")
-    print(f"Arquivo .xlsx final salvo como: {nome_arquivo_excel}")
+            print(f"Número de tabelas encontradas: {lista_tabelas_camelot.n}")
+
+            """
+            if ano == 2020 or ano == 2021 or ano == 2022 or ano == 2023 or ano == 2024:
+                for i, tabela_atual in enumerate(lista_tabelas_camelot):
+                    print(f"\n--- Tabela {i+1} ---")
+                    
+                    df_atual = tabela_atual.df
+                    
+                    print(df_atual.head())
+                    
+                    if len(df_atual.columns) > 3: df_atual = df_atual.loc[:, :2]
+                    
+                    lista_de_dfs.append(df_atual)
+            """
+            if ano == 2025:
+                for i, tabela_atual in enumerate(lista_tabelas_camelot):
+                    print(f"\n--- Tabela {i+1} ---")
+                    
+                    df_atual = tabela_atual.df
+                    print(df_atual.head())
+                    
+                    lista_de_dfs.append(df_atual)
+                
+        tabela_final = pd.concat(lista_de_dfs, ignore_index=True)
+        print(tabela_final.columns)
+        print(tabela_final.head())
+        tabela_final.columns = ["Ato", "Data", "Envolvidos", "Texto"]
+
+        nome_arquivo_csv = f"./atos_csv/atos_{ano}.csv"
+        nome_arquivo_excel = f"./atos_excel/atos_{ano}.xlsx"
+
+        tabela_final.to_csv(nome_arquivo_csv, index=False, encoding='utf-8')
+        tabela_final.to_excel(nome_arquivo_excel)
+
+        print("\n-------------------------------------")
+        print(f"Total de linhas na tabela final: {len(tabela_final)}")
+        print(f"Arquivo .csv final salvo como: {nome_arquivo_csv}")
+        print(f"Arquivo .xlsx final salvo como: {nome_arquivo_excel}")
--- data_prep/to_md.py
+++ data_prep/to_md.py
@@ -1,44 +1,76 @@
+import os
+import re
 import pandas as pd
 import numpy as np
 
+
+def normalizar(match):
+        original = match.group(0)
+        normalizado = re.sub(r"[./,-]", "", original)
+        return f"{normalizado}"
+
+
+def normalizar_siape(texto):
+    padrao = r"\b\d{7},"
+    return re.sub(padrao, normalizar, texto)
+
+    
+def normalizar_processos(texto):
+    padrao = r"\b\d{5}\.\d{6}/\d{4}-\d{2}\b"
+    return re.sub(padrao, normalizar, texto)
+
+
 if __name__ == "__main__":
-    df = pd.read_excel("./atos_excel/atos_2025.xlsx")
-    atos = []
-    i = 0
     
-    while i < len(df):
-        line = df.iloc[i]
-        nmr_ato = line["Ato"]
+    atos_path = "./atos_excel/"
+    for ato in os.listdir(atos_path):
+        df = pd.read_excel(atos_path+ato)
+        atos = []
+        i = 0
+        
+        while i < len(df):
+            line = df.iloc[i]
+            nmr_ato = line["Ato"]
 
-        # Ignora linhas totalmente vazias ou NaN
-        if pd.isna(nmr_ato) or str(nmr_ato).strip() == "":
-            i += 1
-            continue
+            # Ignora linhas totalmente vazias ou NaN
+            if pd.isna(nmr_ato) or str(nmr_ato).strip() == "":
+                i += 1
+                continue
 
-        data = line["Data"]
-        if isinstance(data, str):
-            data = data.replace("\n", "")
-        envolvido = line["Envolvido"]
-        texto = str(line["Texto"]) if not pd.isna(line["Texto"]) else ""
+            data = line["Data"]
+            if isinstance(data, str):
+                data = data.replace("\n", "")
+                data = data.split("/")
+                data = f"{data[1]}-{data[0]}-{data[2]}"
+            texto = str(line["Texto"]) if not pd.isna(line["Texto"]) else ""
+            
+            # Se a próxima linha for do mesmo ato (Ato NaN), junta o texto
+            if i + 1 < len(df):
+                next_line = df.iloc[i + 1]
+                if pd.isna(next_line["Ato"]) or str(next_line["Ato"]).strip() == "":
+                    next_texto = str(next_line["Texto"]) if not pd.isna(next_line["Texto"]) else ""
+                    texto += " " + next_texto
+                    i += 1  # pula a próxima linha, já usada
 
-        # Se a próxima linha for do mesmo ato (Ato NaN), junta o texto
-        if i + 1 < len(df):
-            next_line = df.iloc[i + 1]
-            if pd.isna(next_line["Ato"]) or str(next_line["Ato"]).strip() == "":
-                next_texto = str(next_line["Texto"]) if not pd.isna(next_line["Texto"]) else ""
-                texto += " " + next_texto
-                i += 1  # pula a próxima linha, já usada
-
-        ato = (
-            f"# Ato {len(atos) + 1}\n"
-            f"Numero do ato: {nmr_ato}\n"
-            f"Data do ato: {data}\n"
-            f"Envolvido(s) no ato: {envolvido}\n"
-            f"Texto do ato: {texto.strip()}\n\n"
-        )
-        atos.append(ato)
-        i += 1  # passa para o próximo ato principal
-    
-    # Salva em arquivo
-    with open("atos_md.txt", "w", encoding="utf-8") as arquivo:
-        arquivo.writelines(atos)
+            if len(texto.split("\n")[0]) == 1: texto = texto[1:]
+            texto = (" ").join(texto.split("\n"))
+            
+            texto = normalizar_processos(texto)
+            texto = normalizar_siape(texto)
+            
+            nmr_ato = nmr_ato.strip() if type(nmr_ato) == "str" else nmr_ato
+            
+            ato = (
+                f"# Ato {int(nmr_ato)}\n"
+                f"Numero do ato: {int(nmr_ato)} "
+                f"Data do ato: {data} "
+                f"Texto do ato: {texto.strip()} "
+            )
+            atos.append(ato)
+            i += 1  # passa para o próximo ato principal
+        
+        # Salva em arquivo
+        with open("atos_md.txt", "w", encoding="utf-8") as arquivo:
+            for ato in atos:
+                arquivo.write(f"{ato}\n\n")
+            #arquivo.writelines(atos)
